e7bab81000236902f45df6ab15bd80c97d289367
kitu <kristoftunner95@gmail.com>
2025-03-18 07:41:31 +0100

    updateing setup script

--- setup.sh
+++ setup.sh
@@ -10,6 +10,14 @@ rm -rf ~/.config/nvim
 rm -rf ~/.local/share/nvim
 cp -r ./nvim ~/.config/
 
+echo "Installing vimrc"
+cp ./.vimrc ~/.vimrc
+
+echo "Installing tmux"
+sudo apt update
+sudo apt install install tmux -y
+cp ./.tmux.conf ~/.tmux.conf
+
 echo "installing neovim"
 sudo apt remove nvim
 wget $HOME/.local/bin/nvim https://github.com/neovim/neovim/releases/download/v0.10.4/nvim-linux-x86_64.appimage
