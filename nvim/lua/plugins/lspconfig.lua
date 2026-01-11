4875dbb6e8504717c33f5c3bcbd4db2cb3cb53ed
kitu <kristoftunner95@gmail.com>
2026-01-11 10:27:41 +0100

    implementing the new lsp config

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,13 +1,31 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = {
-      "vim",
-      "lua",
-      "vimdoc",
-      "cpp",
-      "python",
-      "json",
+  {
+    "neovim/nvim-lspconfig",
+  },
+  {
+    "mason-org/mason.nvim",
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = { "clangd", "ruff", "lua_ls", "mypy", "clang-format", "black" },
+    },
+    dependencies = {
+      { "mason-org/mason.nvim", opts = {} },
+      "neovim/nvim-lspconfig",
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "cpp",
+        "python",
+        "json",
+      },
     },
   },
 }
