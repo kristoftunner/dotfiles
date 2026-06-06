99493bde2be6fce8c1108447e89414d454bad3d4
kitu <kristoftunner95@gmail.com>
2026-06-06 06:53:06 +0200

    adding claude settings install

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
+script_dir="$(dirname "$0")"
+
 echo "Installing fzf"
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
-cp exports ~/.exports
+cp $script_dir/exports ~/.exports
 echo "source ~/.exports" >> ~/.bashrc
 echo "export PATH=~/.local/bin:\$PATH" >> ~/.bashrc
 source ~/.bashrc
@@ -14,16 +16,16 @@ echo "removing neovim configs"
 rm -rf ~/.config/nvim
 rm -rf ~/.local/share/nvim
 mkdir ~/.config
-cp -r ./nvim ~/.config/nvim
+cp -r $script_dir/nvim ~/.config/nvim
 
 echo "Installing vimrc"
-cp ./.vimrc ~/.vimrc
+cp $script_dir/.vimrc ~/.vimrc
 
 echo "Installing tmux"
 sudo apt update
 sudo apt install install tmux fd-find libfuse2 xclip -y
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-cp ./.tmux.conf ~/.tmux.conf
+cp $script_dir/.tmux.conf ~/.tmux.conf
 
 echo "Installing yazi"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
@@ -32,10 +34,10 @@ cargo install --locked yazi-fm yazi-cli
 
 mkdir ~/.config/yazi
 git clone https://github.com/yazi-rs/flavors.git ~/.config/yazi/flavors
-cp ./theme.toml ~/.config/yazi/
+cp $script_dir/theme.toml ~/.config/yazi/
 
 echo "Installing aliases"
-cp .aliases ~/.aliases
+cp $script_dir/.aliases ~/.aliases
 echo "source ~/.aliases" >> ~/.bashrc
 echo "source ~/.aliases" >> ~/.zshrc
 
@@ -48,3 +50,10 @@ chmod +x ~/.local/bin/nvim
 
 nvim ~/.config/nvim
 echo "Make sure to update neovim plugins with Lazy and install LSP from Mason"
+
+echo "Installing Claude Code settings"
+if [[ -d ~/.claude ]]; then
+    rm -rf ~/.claude
+    mkdir -p ~/.claude
+fi
+cp $script_dir/claude/settings.json ~/.claude/settings.json
